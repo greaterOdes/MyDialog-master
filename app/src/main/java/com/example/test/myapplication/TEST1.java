89964aa3c5304b0d115601cce04ca75858b98066
xiaonima666 <bradyfreeloop@163.com>
2020-01-18 13:58:03 +0800

    删除测试文件

--- app/src/main/java/com/example/test/myapplication/TEST1.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.myapplication;
-
-/**
- * Created by Administrator on 2020/1/17 0017.
- */
-
-public class TEST1 {
-//    合并测试
-}
--- app/src/main/java/com/example/test/myapplication/Test22.java
+++
@@ -1,5 +0,0 @@
-package com.example.test.myapplication;
-
-public class Test22 {
-    ///..
-}
